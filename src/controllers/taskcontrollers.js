fc89b7e768559555592c288e148f81536bc6b71f
nabilalharethi <nabilalharethi@gmail.com>
2026-01-05 23:18:12 +0100

    added get task by id

--- src/controllers/taskcontrollers.js
+++ src/controllers/taskcontrollers.js
@@ -14,3 +14,21 @@ const getAllTasks = async (req, res) => {
 
     }   
 };
+
+// get task by id
+const getTaskById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [tasks] = await pool.query('SELECT * FROM tasks WHERE id = ?', [id]);
+
+        if (tasks.length === 0) {
+            return res.status(404).json({ error: 'Task not found' , details: `No task found with id ${id}`});
+            }
+
+    
+        res.status(200).json(tasks[0]);
+    } catch (error) {
+        console.error('Error fetching task:', error);
+        res.status(500).json({ error: 'Internal server error', details: error.message });
+    } 
+};
